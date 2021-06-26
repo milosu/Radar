58cf82245a6c0720c254a34428e75e41516f0136
milosu <milosusljebrka@outlook.com>
2021-06-26 18:00:05 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    container: {
+      center: true,
+      padding: '2em',
+      maxWidth: '100%',
+    },
+    colors: {
+      gray: '#F5F7FB',
+      white: '#ffffff',
+      primary: '#3878E5',
+      disabled: '#9DA5B0',
+      secondary: '#1A2848',
+    },
+    fontFamily: {
+      'poppins': ['Poppins', 'sans-serif']
+     }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
